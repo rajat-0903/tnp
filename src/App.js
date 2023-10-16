72364e8f1a4148dc8b1d212448231372cb9fc460
rajat-0903 <148106851+rajat-0903@users.noreply.github.com>
2023-10-16 21:36:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './Components/Navbar';
+import SearchBar from './Components/SearchBar';
+import Index from './pages/Index';
+import Post from './pages/Post';
+import About from './pages/About';
+
+
+function App() {
+  return (
+    <div>
+      {/* <Index/> */}
+      {/* <Post/> */}
+      <About/>
+    </div>
+
+  );
+}
+
+export default App;
